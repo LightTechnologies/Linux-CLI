d80663929d3dde6c39fb06b5b8492e6457f29518
Harry <59972716+HarryTq@users.noreply.github.com>
2021-05-13 23:43:27 +0100

    Initial project

---
+++ LightVPN.CLI/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LightVPN.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
